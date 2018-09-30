feabaa4fb6f8b3cd6aeee39e4a9084ee8828065f
Nikhil Javeri <nikhil@embodyvr.co>
2018-09-29 21:14:59 -0700

    Algorithms/week_1 addition bit done.

--- Algorithms/week_1/karatsubas_multiplication/main.cpp
+++ Algorithms/week_1/karatsubas_multiplication/main.cpp
@@ -7,7 +7,11 @@
 
 using namespace std;
 
-vector<int> add_num(const vector<int> &x, const vector<int> &y){
+vector<int> add_num(vector<int> &x, vector<int> &y){
+
+    // Reverse the numbers to have efficient looping
+    std::reverse(x.begin(), x.end());
+    std::reverse(y.begin(), y.end());
 
     // Create dummy vectors for x and y with same size
     vector<int> copy_x ((x.size() >= y.size()? x.size(): y.size()), 0);
@@ -21,10 +25,6 @@ vector<int> add_num(const vector<int> &x, const vector<int> &y){
     for(i = 0; i < y.size(); i++)
         copy_y[i] = y[i];
 
-    // Reverse the numbers to have efficient looping
-    std::reverse(copy_x.begin(), copy_x.end());
-    std::reverse(copy_y.begin(), copy_y.end());
-
     // Declare output vector which can be maximum 1 element bigger than the max of input vectors
     vector<int> out ((x.size() >= y.size()? x.size(): y.size()) + 1, 0);
 
@@ -34,15 +34,17 @@ vector<int> add_num(const vector<int> &x, const vector<int> &y){
     // Start from the end of the vector
     for(i = 0; i < copy_x.size(); i++){
         // Add the elements and carry and set the carry to suitable value
-        out[i] = x[i] + y[i] + carry;
+        out[i] = copy_x[i] + copy_y[i] + carry;
         carry = out[i]/10;
         out[i] = out[i]%10;
     }
     // Take care of the last element 
-    out[++i] += carry;
+    out[i] += carry;
 
-    // Reverse the out vector
+    // Reverse the out vector and input vectors
     std::reverse(out.begin(), out.end());
+    std::reverse(x.begin(), x.end());
+    std::reverse(y.begin(), y.end());
 
     return out;
 }
@@ -64,9 +66,16 @@ int main(){
     for(int i = 0; i < len_x; i++){
         vec_x[i] = int(x[i] - '0');
         vec_y[i] = int(y[i] - '0');
-        cout << vec_x[i] << " " << vec_y[i] << endl;
+        // cout << vec_x[i] << " " << vec_y[i] << endl;
     }
-    
+
+    // Test add num
+    vector<int> sum_x (4,8);
+    vector<int> sum_y (3,4);
+    vector<int> sum_xy = add_num(sum_x, sum_y);
+    for(int i = 0; i < sum_xy.size(); i++)
+        cout << sum_xy[i] << endl;
+     
     
     return 1;
 }
